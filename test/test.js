7a66d8b6b9ebf0bd77dbea6bf5c1b6923674218a
Sam Thomson <samt@samt.st>
2018-03-01 11:09:04 +0700

    tests

---
+++ test/test.js
@@ -0,0 +1,9 @@
+'use strict';
+var expect = require('chai').expect;
+var index = require('../dist/index.js');
+
+describe('getCryptoUSDValue function test', async () => {
+    it('should run this test', () => {
+        expect(true)
+    });
+});
